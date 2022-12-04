6ee2223e735be7d3b3a02e85194ab1f1d5712d78
Mateusz1357 <107367815+Mateusz1357@users.noreply.github.com>
2022-12-04 20:59:24 +0100

    Update all.py

--- all.py
+++ all.py
@@ -25,28 +25,28 @@ pin_reset=27
 )
 
 try:
-while True:
-now = datetime.utcnow()+timedelta(hours=1)
-data = pms5003.read()
-url = “https;//spaliny-backedn.onrender.com/api/add/measure”
-headers = {‘Content-type:’application/json’)
-data_obj=
-{
-DeviceCode: get_secret("DeviceCode")
-"pm1_0_particles”:data.data[0],
-“pm2_5_particles”:data.data[1],
-“pm10_particles”:data.data[2],
-“pm1_0_atmos”:data.data[3],
-“pm2_5_atmos”:data.data[4],
-“pm110_atmos”:data.data[5],
-“air_0_3”:data.data[6]
-“air_0_5”:data.data[7]
-“air_1_0”:data.data[8]
-“air_2_5”:data.data[9]
-“air_5_0”:data.data[10]
-“air_10”:data.data[11]
-}
-r = requests.post(url, data=json.dumps(data_obj),headers=headers)
+  while True:
+    now = datetime.utcnow()+timedelta(hours=1)
+    data = pms5003.read()
+    url = “https;//spaliny-backedn.onrender.com/api/add/measure”
+    headers = {‘Content-type:’application/json’)
+    data_obj=
+    {
+    DeviceCode: get_secret("DeviceCode")
+    "pm1_0_particles”:data.data[0],
+    “pm2_5_particles”:data.data[1],
+    “pm10_particles”:data.data[2],
+    “pm1_0_atmos”:data.data[3],
+    “pm2_5_atmos”:data.data[4],
+    “pm110_atmos”:data.data[5],
+    “air_0_3”:data.data[6]
+    “air_0_5”:data.data[7]
+    “air_1_0”:data.data[8]
+    “air_2_5”:data.data[9]
+    “air_5_0”:data.data[10]
+    “air_10”:data.data[11]
+    }
+    r = requests.post(url, data=json.dumps(data_obj),headers=headers)
 
 except KeyboardInterrupt:
 pass
